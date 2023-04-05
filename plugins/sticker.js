847ea2408a1bd1d55da90a4e1372d717a1986eec
ɪ ɴ ʀ ʟ <fasweehmon2@gmail.com>
2023-04-06 00:11:11 +0530

    fix

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -1,18 +1,18 @@
 const { delay } = require("@adiwajshing/baileys");
-const bots = require("../lib/perfix");
+const {inrl} = require("../lib/");
 const { isUrl } = require("../lib/Function");
-const lang = bots.getString("whats_bot");
 const fs = require("fs");
 const path = require("path");
 const {getVar} = require('../lib/database/variable');
 
-bots.inrl(
+inrl(
   {
-    pattern: [ "sticker", "s"],
+    pattern: [ "sticker"],
     desc: "It cnvert image to sticker",
     sucReact: "🔁",
     category: ["all", "create"],
-    type : 'converter'
+    type : 'converter',
+    usage : "to convert short video or image to sticker fromate, ex:- sticker[repleyed_msg]"
   },
   async (message, client) => {
   let data = await getVar();
@@ -26,66 +26,6 @@ bots.inrl(
           packname: STICKER_DATA.split(',')[1],
           categories: ["😄", "😊"],
         });
-      } else if (/image|video|sticker/.test(message.client.mime)) {
-        let download = await message.download();
-        client.sendFile(message.from, download, "", message, {
-          asSticker: true,
-          author: STICKER_DATA.split(',')[0],
-          packname: STICKER_DATA.split(',')[1],
-          categories: ["😄", "😊"],
-        });
-      } else if (message.quoted && message.quoted.mentions[0]) {
-        let url = await client.profilePictureUrl(
-          message.quoted.mentions[0],
-          "image"
-        );
-        client.sendFile(message.from, url, "", message, {
-          asSticker: true,
-          author: STICKER_DATA.split(',')[0],
-          packname: STICKER_DATA.split(',')[1],
-          categories: ["😄", "😊"],
-        });
-      } else if (isUrl(message.client.text)) {
-        if (isUrl(message.client.text))
-          client.sendFile(
-            message.from,
-            isUrl(message.client.text)[0],
-            "",
-            message,
-            {
-              asSticker: true,
-              author: STICKER_DATA.split(',')[0],
-              packname: STICKER_DATA.split(',')[1],
-              categories: ["😄", "😊"],
-            }
-          );
-        else {
-          global.catchError = true;
-          return await client.sendErrorMessage(
-            message.from,
-            "No Url Match",
-            message.key,
-            message
-          );
-        }
-      } else if (message.client.text) {
-        let fetch = await fetchUrl(
-          global.api(
-            "zenz",
-            "/searching/stickersearch",
-            { query: message.client.text },
-            "apikey"
-          )
-        );
-        for (let url of fetch.result) {
-          await delay(1000);
-          client.sendFile(message.from, url, "", message, {
-            asSticker: true,
-            author: STICKER_DATA.split(',')[0],
-            packname: STICKER_DATA.split(',')[1],
-            categories: ["😄", "😊"],
-          });
-        }
       } else if (message.quoted && message.quoted.type == "templateMessage") {
         let _message =
           message.quoted.imageMessage || message.quoted.videoMessage;
@@ -107,22 +47,15 @@ bots.inrl(
           categories: ["😄", "😊"],
         });
       } else {
-        global.catchError = true;
-        return await client.sendErrorMessage(
-          message.from,
-          "Reply to Supported media With Caption",
-          message.key,
-          message
+        return await message.send(
+          "Reply to Supported media With Caption"
         );
       }
-      global.catchError = false;
     } catch (error) {
-      global.catchError = true;
-      return await client.sendErrorMessage(
-        message.from,
-        error,
-        message.key,
-        message
+      return await message.send(
+        JSON.stringify(
+        error
+        )
       );
     }
   }
